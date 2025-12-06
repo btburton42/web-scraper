9000b1b155a1d3dd92ec145ebca8ef5ed2b5212a
Tyler Burton <tylerburton@Mac.lan>
2025-12-06 00:34:39 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## File Downloader
+
+A repo to download some track off Jack Tuttle's guitar block. Adapt as necessary.
+
---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+url = "https://www.jacktuttle.com/product/guitarcollection/?attribute_pa_format=both"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+import os 
+
+def normalize_filename(text):
+    return text.replace(' ', '_').replace('/', '_').strip()
+
+mp3_urls = []
+def main():
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for link in soup.find_all('a', href=True):
+        href = link['href']
+        if re.search(r'\.mp3$', href):
+            rq = requests.get(href, headers=headers)
+            try:
+                with open(os.path.abspath(f"./tracks/{normalize_filename(link.text)}.mp3"), "wb") as file:
+                    file.write(rq.content)
+                    print(f"wrote {normalize_filename(link.text)}.mp3 to disk")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
